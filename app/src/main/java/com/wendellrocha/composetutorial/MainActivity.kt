47b25c13169cc4c538cac0dcaa684640e5fcc571
Wendell Rocha <wendellrocha@outlook.com>
2022-09-12 11:25:01 -0300

    Finished Jetpack Compose basic concepts.

--- app/src/main/java/com/wendellrocha/composetutorial/MainActivity.kt
+++ app/src/main/java/com/wendellrocha/composetutorial/MainActivity.kt
@@ -1,23 +1,33 @@
 package com.wendellrocha.composetutorial
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.wendellrocha.composetutorial.models.Message
+import com.wendellrocha.composetutorial.data.SampleData
 import com.wendellrocha.composetutorial.ui.theme.ComposeTutorialTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,7 +46,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
@@ -54,7 +63,16 @@ fun MessageCard(msg: Message) {
         )
         // add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp))
-        Column {
+
+        // we keep track if the message is expanded or not in this variable
+        var isExpanded by remember { mutableStateOf(false) }
+
+        // surface color will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = msg.author,
                 color = MaterialTheme.colors.secondaryVariant,
@@ -62,10 +80,21 @@ fun MessageCard(msg: Message) {
             )
             // add a vertical space between the author and message
             Spacer(modifier = Modifier.height(4.dp))
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium, elevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change de Surface size gradually
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(all = 1.dp)
+            ) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
+                    // if the message is expanded, we display all its content,
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.body2
                 )
 
@@ -73,7 +102,7 @@ fun MessageCard(msg: Message) {
         }
     }
 }
-
+/*
 @Preview(
     name = "Light Mode",
     showBackground = true
@@ -93,7 +122,23 @@ fun DefaultPreview() {
                     "Hey, take a look at Jetpack Compose, it's great"
                 )
             )
-
         }
+    }
+}*/
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(msg = message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    ComposeTutorialTheme {
+        Conversation(messages = SampleData.conversationSample)
     }
 }
---
+++ app/src/main/java/com/wendellrocha/composetutorial/models/Message.kt
@@ -0,0 +1,3 @@
+package com.wendellrocha.composetutorial.models
+
+data class Message(val author: String, val body: String)
